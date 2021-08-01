390d5a3506d35dfbc8b77abccb5cf0a83e0d5ebc
Juan <jd.camayo@gmail.com>
2021-08-01 10:28:50 -0500

    search component added

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../components/Header'
 import '../assets/styles/App.scss'
+import Search from '../components/Search'
 
 function App() {
     return (
         <div className='App'>
             <Header />
+            <Search />
         </div>
     )
 }
