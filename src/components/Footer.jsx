b51c021f0bbf99afcdaa8f917c28bc24e4767aad
Juan <jd.camayo@gmail.com>
2021-08-07 22:10:34 -0500

    adding hooks

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import '../assets/styles/components/Footer.scss'
 
 function Footer() {
     return (
-        <footer class='footer'>
+        <footer className='footer'>
             <a href='/'>Terminos de uso</a>
             <a href='/'>Declaración de privacidad</a>
             <a href='/'>Centro de ayuda</a>
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Header from '../components/Header'
 import Search from '../components/Search'
 import Categories from '../components/Categories'
@@ -9,6 +9,16 @@ import Footer from '../components/Footer'
 import '../assets/styles/App.scss'
 
 function App() {
+    const [videos, setVideos] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:3000/initialState')
+            .then(response => response.json())
+            .then(data => setVideos(data))
+    }, [])
+
+    console.log(videos)
+    
     return (
         <div className='App'>
             <Header />
